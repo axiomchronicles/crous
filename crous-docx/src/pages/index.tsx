90ca1a200aeb735f22c54eb8e23c8cf5e379499c
kuroyami <aegis.invincible@gmail.com>
2025-12-07 21:26:25 +0530

    Initial Commit 04

--- crous-docx/src/pages/index.tsx
+++ crous-docx/src/pages/index.tsx
@@ -27,7 +27,7 @@ function HomepageHeader() {
           <div className={styles.buttons}>
             <Link
               className="button button--secondary button--lg"
-              to="/">
+              to="/docs/">
               Get Started →
             </Link>
             <Link
